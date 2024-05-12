62d4277cca4f742a7e3fc5ed8a449e188a7d7a86
Mahiballah <145888808+mahibullah11@users.noreply.github.com>
2024-05-12 11:18:02 +0500

    done with to do list app

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const todoList = document.getElementById('todoList');
 const addTodo = ()=>{
    const inputText = inputBox.value.trim();
    if(inputText.length <= 0){
-    alert("You must wirte something n your to do");
+    alert("You must write something in your to do list");
     return false;
    }
    //create p tag
@@ -19,9 +19,21 @@ const addTodo = ()=>{
    //deleteBtn
    const deleteBtn = document.createElement("button");
    deleteBtn.innerText = "Delete";
+   deleteBtn.classList.add("btn", "deleteBtn");
    li.appendChild(deleteBtn);
 
    todoList.appendChild(li);
    inputBox.value = "";
 }
+
+
+const updateTodo = (e)=>{
+   console.log(e.target.innerHTML);
+   if(e.target.innerHTML === "Delete"){
+      console.log(e.target.parentElement);
+      todoList.removeChild(e.target.parentElement);
+   }
+}
+
 addBtn.addEventListener('click', addTodo);
+todoList.addEventListener('click', updateTodo);
